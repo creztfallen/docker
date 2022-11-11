699bcf311eb2322f07cda4feb6ed3f0c26d81e90
root <root@mateus-moreno.FLEX.LOCAL>
2022-11-11 09:16:47 -0300

    connectng node with mysql

--- node/index.js
+++ node/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
+const connection = mysql.createConnection(config);
+
+// const sql = `INSERT INTO people(name) values('Geralt')`
+// connection.query(sql);
+// connection.end();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
-    res.send('<h1>This app is running without node on my machine. Docker is awesome!</h1>')
+    res.send('<h1>This app is running without node on my machine. Docker is awesome!</h1>');
 })
 
-app.listen(port, () => console.log(`App running on port ${port}`))
+app.post('/people', (req, res) => {
+    connection.query(`INSERT INTO people(name) values('Yennefer')`);
+    res.send('The wind is howling.');
+})
+
+app.listen(port, () => console.log(`App running on port ${port}`));
